935d1f35d1d7388741b37d8753ee546b8113b459
Gabriel Ozeas <gabriel.ozeas1@gmail.com>
2013-05-11 07:56:58 -0300

    Adding client node tests

---
+++ test/spec/client/NodeSpec.js
@@ -0,0 +1,57 @@
+define(['main'], function() {
+	describe('Node', function() {
+		var line;
+
+		beforeEach(function() {
+			line = new Line(null, {x: 0, y:0});
+		});
+
+		it('create a new node position in center', function() {
+			node = new Node(line, {x: 20, y:30});
+
+			expect(node.container).toBe(line);
+			expect(node.position.x).toBe(20);
+			expect(node.position.y).toBe(30);
+		});
+
+		it('linking two nodes should save links', function() {
+			nodeOne = new Node(line, {x: 20, y:30});
+			nodeTwo = new Node(line, {x: 40, y:50});
+
+			nodeOne.linkWith(nodeTwo);
+
+			expect(nodeOne.fromLinks.length).toBe(1);
+			expect(nodeOne.toLinks.length).toBe(0);
+
+			expect(nodeTwo.toLinks.length).toBe(1);
+			expect(nodeTwo.fromLinks.length).toBe(0);
+		});
+
+		it('moving node should change the its position', function() {
+			node = new Node(line, {x: 20, y:30});
+
+			expect(node.position.x).toBe(20);
+			expect(node.position.y).toBe(30);
+
+			node.move({x: 40, y: 50});
+
+			expect(node.position.x).toBe(40);
+			expect(node.position.y).toBe(50);
+		});
+
+		it('nodes from same line should be at same container', function(){
+			var nodeOne = new Node(line, {x: 20, y:30});
+			var nodeTwo = new Node(line, {x: 30, y:40});
+
+			expect(nodeOne.isFromSameContainer(nodeTwo)).toBe(true);
+			expect(nodeTwo.isFromSameContainer(nodeOne)).toBe(true);
+
+			var nodeThree = new Node(new Line(null, {x: 0, y: 0}), {x: 0, y: 0});
+
+			expect(nodeOne.isFromSameContainer(nodeThree)).toBe(false);
+			expect(nodeThree.isFromSameContainer(nodeOne)).toBe(false);
+		});
+
+
+	});
+});
